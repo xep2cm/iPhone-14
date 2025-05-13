b29aad006a03f1f32b2aeebeef9d978f9b468957
xep2cm <whitebeatstore@gmail.com>
2025-05-13 17:22:50 +0500

    commit_2

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.menu__btn');
-const menu = document.querySelector('.menu__list');
-
-menuBtn.addEventListener('click', ()=>{
-    menu.classList.toggle('menu--active');
-})
